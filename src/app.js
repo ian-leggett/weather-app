9353e01116bd7f81b7c92b905fddcc8c2e4cb4a6
Ian Leggett <contactleggett@gmail.com>
2019-07-15 19:44:28 +0100

    Updated port number to use process port

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDir = path.join(__dirname, '../public')
 const viewsDir = path.join(__dirname, '../src/templates/views')
@@ -80,6 +81,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server running on port 3000')
 })
